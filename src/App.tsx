9d5686a54231c1c943a426ec43a782564b240eb3
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2024-03-02 15:36:05 +0100

    sort games

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,68 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-import { useState } from "react";
-import { Genre } from "./hooks/useGenres";
-import PlatformSelector from "./components/PlatformSelector";
-import { Platform } from "./hooks/useGames";
-import SortSelector from "./components/SortSelector";
+import { Grid, GridItem, HStack, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
+import GameGrid from './components/GameGrid';
+import GenreList from './components/GenreList';
+import { useState } from 'react';
+import { Genre } from './hooks/useGenres';
+import PlatformSelector from './components/PlatformSelector';
+import { Platform } from './hooks/useGames';
+import SortSelector from './components/SortSelector';
 
 export interface GameQuery {
-  genre: Genre | null;
-  platform: Platform | null;
+	genre: Genre | null;
+	platform: Platform | null;
+	sortOrder: string;
 }
 
 function App() {
-  // const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  // const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
+	// const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+	// const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
 
-  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+	const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
-  return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-      templateColumns={{
-        base: "1fr",
-        lg: "200px 1fr",
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" paddingX={"5"}>
-          <GenreList
-            selectedGenre={gameQuery.genre}
-            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
-          />
-        </GridItem>
-      </Show>
-      <GridItem area="main">
-        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-          <PlatformSelector
-            selectedPlatform={gameQuery.platform}
-            onSelectPlatform={(platform) =>
-              setGameQuery({ ...gameQuery, platform })
-            }
-          />
-          <SortSelector />
-        </HStack>
-        <GameGrid gameQuery={gameQuery} />
-      </GridItem>
-    </Grid>
-  );
+	return (
+		<Grid
+			templateAreas={{
+				base: `"nav" "main"`,
+				lg: `"nav nav" "aside main"`,
+			}}
+			templateColumns={{
+				base: '1fr',
+				lg: '200px 1fr',
+			}}
+		>
+			<GridItem area="nav">
+				<NavBar />
+			</GridItem>
+			<Show above="lg">
+				<GridItem area="aside" paddingX={'5'}>
+					<GenreList
+						selectedGenre={gameQuery.genre}
+						onSelectGenre={(genre) =>
+							setGameQuery({ ...gameQuery, genre })
+						}
+					/>
+				</GridItem>
+			</Show>
+			<GridItem area="main">
+				<HStack spacing={5} paddingLeft={2} marginBottom={5}>
+					<PlatformSelector
+						selectedPlatform={gameQuery.platform}
+						onSelectPlatform={(platform) =>
+							setGameQuery({ ...gameQuery, platform })
+						}
+					/>
+					<SortSelector
+						sortOrder={gameQuery.sortOrder}
+						onSelectSortOrder={(sortOrder) =>
+							setGameQuery({ ...gameQuery, sortOrder })
+						}
+					/>
+				</HStack>
+				<GameGrid gameQuery={gameQuery} />
+			</GridItem>
+		</Grid>
+	);
 }
 
 export default App;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,28 +1,35 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
-import useData from "./useData";
-import { Genre } from "./useGenres";
-import { GameQuery } from "../App";
+import { useState, useEffect } from 'react';
+import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
+import useData from './useData';
+import { Genre } from './useGenres';
+import { GameQuery } from '../App';
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+	id: number;
+	name: string;
+	slug: string;
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-  }
+	id: number;
+	name: string;
+	background_image: string;
+	parent_platforms: { platform: Platform }[];
+	metacritic: number;
+}
 
-const useGames = (gameQuery: GameQuery) => useData<Game>('/games', { 
-    params: { 
-      genres: gameQuery.genre?.id, 
-      platforms: gameQuery.platform?.id 
-    }}, [gameQuery]);
+const useGames = (gameQuery: GameQuery) =>
+	useData<Game>(
+		'/games',
+		{
+			params: {
+				genres: gameQuery.genre?.id,
+				platforms: gameQuery.platform?.id,
+				ordering: gameQuery.sortOrder,
+			},
+		},
+		[gameQuery]
+	);
 
 export default useGames;
